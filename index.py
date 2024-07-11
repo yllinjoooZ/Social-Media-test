0dca1322416eeb1cfb93de99257dc07f30186f40
yllinjoooZ <yllzeneli920@gmail.com>
2024-07-11 02:56:50 -0700

    version 1

---
+++ index.py
@@ -0,0 +1,8 @@
+while True:
+    emri = input("Sheno username:")
+    if emri != "yllinjooo":
+      continue
+    password = input("Shkruaje passwordin:")
+    if password == "jcoders":
+     break
+print("emri dhe passwordi jane ne rregull:")
